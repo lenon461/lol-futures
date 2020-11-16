0f40fbe3f799c8dfc888eaa2d784613bdedf4162
ljs <lenon461@gmail.com>
2020-11-16 20:06:25 +0900

    websocket set

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -2,19 +2,17 @@ import SC from "socket.io-client"
 
 class Socket {
 
-    public socket: any;
+    public socket: WebSocket = new WebSocket('ws://localhost:8090');;
 
 
     init() {
-        const host = "ws://127.0.0.1:8090"
-        this.socket = SC.io(host)
-        this.socket.on("events", (message: any) => {
+        this.socket.onmessage = ((message: any) => {
             console.log(message)
         })
     }
 
     emit() {
-        this.socket.emit("@test", {test: 1})
+        this.socket.send(JSON.stringify({event: "@test", test: 1}))
     }
     
 }
